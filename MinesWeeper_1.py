bed9ee1b1302c3d2742c1347e736cf41ce62b70d
mario69werner <mario69werner@gmail.com>
2020-02-28 18:45:01 +0100

    noch 1 Fehler
    Signed-off-by: mario69werner <mario69werner@gmail.com>

--- MinesWeeper_1.py
+++ MinesWeeper_1.py
@@ -1,8 +1,10 @@
 import pygame as pg
 from dataclasses import dataclass
+import random as rnd
 
-auflösung = 1000
-raster = 10
+auflösung = 500
+raster = 9
+anzMinen = 10
 abstand = auflösung // raster
 
 pg.init()
@@ -15,8 +17,9 @@ for n in range(9):
     cell_selected.append(pg.transform.scale(pg.image.load(f'C:\Sprachen\Python\Lessons\Teil_10_ms_cell_{n}.gif'), (abstand, abstand)))
 
 
-anzMinen = 10
+
 matrix = []
+benachbarteFelder =[(-1, -1), (-1, 0), (-1,  1), (0, -1), (0, 1 ), (1,- 1), (1, 0), (1, 1)]
 
 @dataclass
 class Cell():
@@ -40,7 +43,65 @@ class Cell():
             else:
                 screen.blit(cell_normal, pos)
 
+    def anzMinenErmitteln(self):
+        for pos in benachbarteFelder:
+            neueZeile = self.zeile + pos[0]
+            neueSpalte = self.spalte + pos[0]
+            if neueZeile >= 0 and neueZeile < raster and neueSpalte >= 0 and neueSpalte < raster:
+                if matrix[neueZeile * raster + neueSpalte].mine:
+                    self.anzMinenDrumrum += 1
+            
 
+for n in range(raster * raster):
+    matrix.append(Cell(n // raster, n % raster))
+
+while anzMinen > 0:
+    x = rnd.randrange(raster * raster)
+    if not matrix[x].mine:
+        matrix[x].mine = True
+        anzMinen -= 1
+
+def floodFill(zeile, spalte):
+    for pos in benachbarteFelder:
+        neueZeile = zeile + pos[0]
+        neueSpalte = spalte + pos[1]
+        if neueZeile >= 0 and neueZeile < raster and neueSpalte >= 0 and neueSpalte < raster:
+            cell = matrix[neueZeile * raster + neueSpalte]
+            if cell.anzMinenDrumrum == 0 and not cell.selected:
+                cell.selected = True
+                floodFill(neueZeile, neueSpalte)
+            else:
+                cell.selected = True
+
+
+
+for objekt in matrix:
+    objekt.anzMinenErmitteln()
+
+
+weitermachen = True
+while weitermachen:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            weiteramchen = False
+        if event.type == pg.MOUSEBUTTONDOWN:
+            mouseX, mouseY = pg.mouse.get_pos()
+            zeile = mouseX // abstand
+            spalte = mouseY // abstand
+            i = zeile * raster + spalte
+            cell = matrix[i]
+            if pg.mouse.get_pressed()[2]:
+                cell.flagged = not cell.flagged
+            if pg.mouse.get_pressed()[0]:
+                cell.selected = True
+                if cell.anzMinenDrumrum == 0 and not cell.mine:
+                    floodFill(zeile, spalte)
+                if cell.mine:
+                    for objekt in matrix:
+                        objekt.selected = True
+            
+    for objekt in matrix:
+        objekt.show()
+    pg.display.flip()
 
 pg.quit()
-
